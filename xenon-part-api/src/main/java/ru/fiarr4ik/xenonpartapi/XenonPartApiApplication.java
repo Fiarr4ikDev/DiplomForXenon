09cd493efd35e8e9293f202e5314da7bcbe0a242
Fiarr4ik <fiarr4ik@gmail.com>
2025-06-05 23:08:01 +0700

    init commit

---
+++ xenon-part-api/src/main/java/ru/fiarr4ik/xenonpartapi/XenonPartApiApplication.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.xenonpartapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XenonPartApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XenonPartApiApplication.class, args);
+    }
+
+}
---
+++ xenon-part-api/src/test/java/ru/fiarr4ik/xenonpartapi/XenonPartApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.fiarr4ik.xenonpartapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XenonPartApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
